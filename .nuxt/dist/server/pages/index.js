73b054074c4b38944732a9681a2e59008aee6275
Isaac <isaac.cortez1294@hotmail.com>
2021-11-15 17:26:17 -0600

    Add app

---
+++ .nuxt/dist/server/pages/index.js
@@ -0,0 +1,183 @@
+exports.ids = [1];
+exports.modules = {
+
+/***/ 22:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "img/bg-image.34e84a3.jpg";
+
+/***/ }),
+
+/***/ 23:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// CONCATENATED MODULE: ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./pages/index.vue?vue&type=template&id=3d68dad0&
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"bg-no-repeat bg-cover flex justify-center p-10",style:(_vm.bgImg)},[_vm._ssrNode("<div class=\"\n      grid\n      lg:grid-cols-3 lg:w-10/12 lg:h-3/5\n      md:grid-cols-2 md:w-4/5 md:h-3/5\n      gap-8\n      max-w-screen-lg\n    \">"+(_vm._ssrList((_vm.data),function(items){return ("<div class=\"\n        rounded-md\n        bg-black bg-opacity-50\n        border-2\n        text-white\n        flex flex-col\n        items-center\n        p-10\n        shadow-2xl\n        hover:bg-opacity-80\n        transition\n        duration-500\n        ease-in-out\n        transform\n        hover:-translate-y-1 hover:scale-110\n        gap-6\n        text-center text-xs\n        font-sans\n        md:text-sm\n        xl:text-lg\n      \"><img"+(_vm._ssrAttr("src",items.image))+" class=\"\n          w-20\n          h-20\n          rounded-full\n          object-cover\n          md:h-28 md:w-28\n          lg:w-36 lg:h-36\n        \"> <div class=\"tracking-tight\">"+_vm._ssrEscape("\n        "+_vm._s(items.description)+"\n      ")+"</div> <div class=\"\n          flex flex-row\n          w-full\n          text-center\n          justify-around\n          divide-purple-500 divide-x-2 divide-black\n        \"><div class=\"w-1/2\">"+_vm._ssrEscape(_vm._s(items.name))+"</div> <div class=\"w-1/2\">"+_vm._ssrEscape(_vm._s(items.ocupation))+"</div></div> <button class=\"\n          text-white\n          font-bold\n          py-1\n          px-2\n          rounded\n          bg-gradient-to-r\n          from-purple-500\n          to-purple-400\n        \">\n        More Information\n      </button></div>")}))+"</div>")])}
+var staticRenderFns = []
+
+
+// CONCATENATED MODULE: ./pages/index.vue?vue&type=template&id=3d68dad0&
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib??ref--2-0!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./pages/index.vue?vue&type=script&lang=js&
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+/* harmony default export */ var lib_vue_loader_options_pagesvue_type_script_lang_js_ = ({
+  data() {
+    return {
+      data: [{
+        id: 1,
+        name: "Alfred",
+        description: '"Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus tempora sequi fugit dolorem veritatis incidunt consectetur animi tempore suscipit excepturi itaque accusamus nobis vel, hic numquam"',
+        ocupation: "Developer",
+        image: "https://images.unsplash.com/photo-1521252659862-eec69941b071?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=325&q=80"
+      }, {
+        id: 2,
+        name: "Joel",
+        description: '"Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus tempora sequi fugit dolorem veritatis incidunt consectetur animi tempore suscipit excepturi itaque accusamus nobis vel, hic numquam"',
+        ocupation: "Doctor",
+        image: "https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80"
+      }, {
+        id: 3,
+        name: "Sarah",
+        description: '"Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus tempora sequi fugit dolorem veritatis incidunt consectetur animi tempore suscipit excepturi itaque accusamus nobis vel, hic numquam"',
+        ocupation: "Engineer",
+        image: "https://images.unsplash.com/photo-1628890920690-9e29d0019b9b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+      }, {
+        id: 4,
+        name: "Blanca",
+        description: '"Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus tempora sequi fugit dolorem veritatis incidunt consectetur animi tempore suscipit excepturi itaque accusamus nobis vel, hic numquam"',
+        ocupation: "Engineer",
+        image: "https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80"
+      }, {
+        id: 5,
+        name: "Juan",
+        description: '"Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus tempora sequi fugit dolorem veritatis incidunt consectetur animi tempore suscipit excepturi itaque accusamus nobis vel, hic numquam"',
+        ocupation: "Engineer",
+        image: "https://images.unsplash.com/photo-1544723795-3fb6469f5b39?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=389&q=80"
+      }, {
+        id: 6,
+        name: "Karla",
+        description: '"Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus tempora sequi fugit dolorem veritatis incidunt consectetur animi tempore suscipit excepturi itaque accusamus nobis vel, hic numquam"',
+        ocupation: "Engineer",
+        image: "https://images.unsplash.com/photo-1598550880863-4e8aa3d0edb4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=327&q=80"
+      }],
+      bgImg: {
+        backgroundImage: `url(${__webpack_require__(22)})`
+      }
+    };
+  }
+
+}); //BG IMAGE
+//https://images.unsplash.com/photo-1634270874854-3c7492905b74?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1100&q=80
+// CONCATENATED MODULE: ./pages/index.vue?vue&type=script&lang=js&
+ /* harmony default export */ var pagesvue_type_script_lang_js_ = (lib_vue_loader_options_pagesvue_type_script_lang_js_); 
+// EXTERNAL MODULE: ./node_modules/vue-loader/lib/runtime/componentNormalizer.js
+var componentNormalizer = __webpack_require__(2);
+
+// CONCATENATED MODULE: ./pages/index.vue
+
+
+
+
+
+/* normalize component */
+
+var component = Object(componentNormalizer["a" /* default */])(
+  pagesvue_type_script_lang_js_,
+  render,
+  staticRenderFns,
+  false,
+  null,
+  null,
+  "e2089270"
+  
+)
+
+/* harmony default export */ var pages = __webpack_exports__["default"] = (component.exports);
+
+/***/ })
+
+};;
+//# sourceMappingURL=index.js.map
---
+++ pages/index.vue
@@ -0,0 +1,149 @@
+<template>
+  <div class="bg-no-repeat bg-cover flex justify-center p-10" :style="bgImg">
+    <div
+      class="
+        grid
+        lg:grid-cols-3 lg:w-10/12 lg:h-3/5
+        md:grid-cols-2 md:w-4/5 md:h-3/5
+        gap-8
+        max-w-screen-lg
+      "
+    >
+      <div
+        class="
+          rounded-md
+          bg-black bg-opacity-50
+          border-2
+          text-white
+          flex flex-col
+          items-center
+          p-10
+          shadow-2xl
+          hover:bg-opacity-80
+          transition
+          duration-500
+          ease-in-out
+          transform
+          hover:-translate-y-1 hover:scale-110
+          gap-6
+          text-center text-xs
+          font-sans
+          md:text-sm
+          xl:text-lg
+        "
+        v-for="items of data"
+        :key="items.id"
+      >
+        <img
+          class="
+            w-20
+            h-20
+            rounded-full
+            object-cover
+            md:h-28 md:w-28
+            lg:w-36 lg:h-36
+          "
+          :src="items.image"
+        />
+        <div class="tracking-tight">
+          {{ items.description }}
+        </div>
+        <div
+          class="
+            flex flex-row
+            w-full
+            text-center
+            justify-around
+            divide-purple-500 divide-x-2 divide-black
+          "
+        >
+          <div class="w-1/2">{{ items.name }}</div>
+          <div class="w-1/2">{{ items.ocupation }}</div>
+        </div>
+        <button
+          class="
+            text-white
+            font-bold
+            py-1
+            px-2
+            rounded
+            bg-gradient-to-r
+            from-purple-500
+            to-purple-400
+          "
+        >
+          More Information
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      data: [
+        {
+          id: 1,
+          name: "Alfred",
+          description:
+            '"Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus tempora sequi fugit dolorem veritatis incidunt consectetur animi tempore suscipit excepturi itaque accusamus nobis vel, hic numquam"',
+          ocupation: "Developer",
+          image:
+            "https://images.unsplash.com/photo-1521252659862-eec69941b071?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=325&q=80",
+        },
+        {
+          id: 2,
+          name: "Joel",
+          description:
+            '"Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus tempora sequi fugit dolorem veritatis incidunt consectetur animi tempore suscipit excepturi itaque accusamus nobis vel, hic numquam"',
+          ocupation: "Doctor",
+          image:
+            "https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80",
+        },
+        {
+          id: 3,
+          name: "Sarah",
+          description:
+            '"Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus tempora sequi fugit dolorem veritatis incidunt consectetur animi tempore suscipit excepturi itaque accusamus nobis vel, hic numquam"',
+          ocupation: "Engineer",
+          image:
+            "https://images.unsplash.com/photo-1628890920690-9e29d0019b9b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+        },
+        {
+          id: 4,
+          name: "Blanca",
+          description:
+            '"Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus tempora sequi fugit dolorem veritatis incidunt consectetur animi tempore suscipit excepturi itaque accusamus nobis vel, hic numquam"',
+          ocupation: "Engineer",
+          image:
+            "https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80",
+        },
+        {
+          id: 5,
+          name: "Juan",
+          description:
+            '"Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus tempora sequi fugit dolorem veritatis incidunt consectetur animi tempore suscipit excepturi itaque accusamus nobis vel, hic numquam"',
+          ocupation: "Engineer",
+          image:
+            "https://images.unsplash.com/photo-1544723795-3fb6469f5b39?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=389&q=80",
+        },
+        {
+          id: 6,
+          name: "Karla",
+          description:
+            '"Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus tempora sequi fugit dolorem veritatis incidunt consectetur animi tempore suscipit excepturi itaque accusamus nobis vel, hic numquam"',
+          ocupation: "Engineer",
+          image:
+            "https://images.unsplash.com/photo-1598550880863-4e8aa3d0edb4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=327&q=80",
+        },
+      ],
+      bgImg: {
+        backgroundImage: `url(${require("../assets/images/bg-image.jpg")})`,
+      },
+    };
+  },
+};
+//BG IMAGE
+//https://images.unsplash.com/photo-1634270874854-3c7492905b74?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1100&q=80
+</script>
